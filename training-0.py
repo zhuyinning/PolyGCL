d577a69d280e4d2c7b596bc2b2b880e3934f243a
zhuyinning <zhuyinning0@gmail.com>
2026-02-27 12:05:59 +0800

    Update training-0.py

--- training-0.py
+++ training-0.py
@@ -102,7 +102,6 @@ if __name__ == "__main__":
 
     optimizer = torch.optim.Adam([
         {'params': model.encoder.lin1.parameters(), 'weight_decay': args.wd1, 'lr': args.lr1},
-        {'params': model.disc.parameters(), 'weight_decay': args.wd1, 'lr': args.lr1},
         {'params': model.encoder.prop1.parameters(), 'weight_decay': args.wd, 'lr': args.lr},
         {'params': model.alpha, 'weight_decay': args.wd, 'lr': args.lr},
         {'params': model.beta, 'weight_decay': args.wd, 'lr': args.lr}
