f5cdc6ed3247ec7e62a3c1222365f1c649e30133
zhuyinning <zhuyinning0@gmail.com>
2026-03-01 16:26:47 +0800

    Update training0.py

--- training0.py
+++ training0.py
@@ -40,9 +40,8 @@ parser.add_argument('--dprate', type=float, default=0.5)
 parser.add_argument('--is_bns', type=bool, default=False)
 parser.add_argument('--act_fn', default='relu')
 parser.add_argument("--tau", type=float, default=0.1)
-parser.add_argument("--lambda_graph", type=float, default=0.08)
-parser.add_argument('--lambda_min', type=float, default=0.05)
-parser.add_argument('--margin', type=float, default=0.03)
+parser.add_argument("--lambda_graph", type=float, default=0.5)
+parser.add_argument('--lambda_min', type=float, default=0.01)
 args = parser.parse_args()
 
 if args.gpu != -1 and th.cuda.is_available():
